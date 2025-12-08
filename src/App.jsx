f7a42729ac84aec4be9d3fdc84c04350e2045073
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-12-08 22:21:47 +0630

    useStateSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
+import Search from "./Components/Search.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+    const [searchText, setSearchText] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <img src="/main-bg.jpg" />
+                    <h1 className="text-3xl font-bold">
+                        Find <span
+                        className="bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text text-transparent">Movies</span> You'll
+                        Like
+                    </h1>
+                </header>
+                <Search searchText={searchText} setSearchText={setSearchText} />
+            </div>
+
+
+        </main>
+    )
 }
 
 export default App
---
+++ src/Components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Search = ({searchText, setSearchText}) => {
+    return (
+        <>
+            <div className="flex items-center gap-3 border rounded-xl bg-white shadow-sm w-1/6  ">
+                <img src="/search.jpg" alt="" className="w-10 h-10 object-contain" />
+                <input
+                    className="flex-1 h-10 outline-none"
+                    type="text"
+                    value={searchText}
+                    placeholder="Search..."
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+            </div>
+            <div className="text-gray-800 text-3xl">{searchText}</div>
+        </>
+    )
+}
+export default Search
